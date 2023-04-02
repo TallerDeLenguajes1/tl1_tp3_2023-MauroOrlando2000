b23560174274e8d0afad69584c0b4824a4ad2467
Mauro Orlando <orlandomauro2000@gmail.com>
2023-04-02 12:47:04 -0300

    punto 2 incompleto v1.1

--- tp3_2.c
+++ tp3_2.c
@@ -4,26 +4,23 @@
 #define anio 5
 #define mes 12
 
-//void cargarMatriz(int matriz[anio][mes]);
+void cargarMatriz(int matriz[anio][mes]);
+void mostrarMatriz(int matriz[anio][mes]);
+void promedio(int matriz[anio][mes]);
+void minimoMaximo(int matriz[anio][mes]);
 
 int main()
 {
-    int matriz[anio][mes], i, j;
+    int matriz[anio][mes];
 
-    for(i=0; i<anio; i++)
-    {
-        for(j=0; j<mes; j++)
-        {
-            matriz[i][j] = 10000 + rand() % 50000;
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
+    cargarMatriz(matriz);
+    mostrarMatriz(matriz);
+    promedio(matriz);
 
     return 0;
 }
 
-/*void cargarMatriz(int matriz[anio][mes])
+void cargarMatriz(int matriz[anio][mes])
 {
     int i, j;
     srand(time(NULL));
@@ -35,4 +32,40 @@ int main()
             matriz[i][j] = 10000 + rand() % 50000;
         }
     }
-}*/
+}
+
+void mostrarMatriz(int matriz[anio][mes])
+{
+    int i=0, j;
+    
+    for(i=0; i<anio; i++)
+    {
+        for(j=0; j<mes; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void promedio(int matriz[anio][mes])
+{
+    int i, j, promedio=0;
+
+    printf("\n");
+    for (i=0; i<anio; i++)
+    {
+        for(j=0; j<mes; j++)
+        {
+            promedio += matriz[i][j];
+        }
+        promedio /= 12;
+        printf("promedio anio %d = %d", i+1, promedio);
+        printf("\n");
+    }
+}
+
+void minimoMaximo(int matriz[anio][mes])
+{
+    int i, j, mesMinimo, anioMinimo, mesMaximo, anioMaximo;
+}
